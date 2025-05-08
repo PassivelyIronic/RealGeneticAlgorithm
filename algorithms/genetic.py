3ff53b01db68064be739020aecd2127734c663b5
Michal <ciurej.michal01@gmail.com>
2025-05-08 17:42:15 +0200

    Poprawki techniczne

--- algorithms/genetic.py
+++ algorithms/genetic.py
@@ -1,125 +1,27 @@
 from algorithms.config import config
-from algorithms.selection import best_selection, roulette_selection, tournament_selection
-from algorithms.crossover import arithmetic_crossover, linear_crossover, blend_alpha_crossover, \
-    blend_alpha_beta_crossover, averaging_crossover
 from algorithms.fitness import evaluate_fitness
-from algorithms.individual import Individual
+from algorithms.population import Population
 from apps.plotter import Plotter
-import random
 import time
 
 
 def run_genetic_algorithm():
-    population_size = config.population_size
-    num_epochs = config.epochs
-    crossover_rate = config.crossover_probability
-    mutation_rate = config.mutation_probability
-    elitism_count = config.best_selection_amount
-    selection_method = config.selection_method
-    crossover_method = config.crossover_method
-    num_variables = config.num_variables
-    precision = config.precision
     bounds = (config.range_start, config.range_end)
-
     plotter = Plotter()
     start_time = time.time()
 
-    population = [Individual(num_variables=num_variables, precision=precision) for _ in range(population_size)]
+    population = Population(config)
 
-    for ind in population:
-        ind.evaluate(evaluate_fitness, bounds)
+    population.evaluate_all(evaluate_fitness, bounds)
 
-    if config.optimization_type == "max":
-        best_individual = max(population, key=lambda ind: ind.fitness)
-        optimization_func = max
-    else:
-        best_individual = min(population, key=lambda ind: ind.fitness)
-        optimization_func = min
+    plotter.update_history(population.best_individual.fitness, population.avg_fitness_history[0])
+    print(f"Epoka 0, najlepsze przystosowanie: {population.best_individual.fitness}")
 
-    avg_fitness = sum(ind.fitness for ind in population) / len(population)
-    plotter.update_history(best_individual.fitness, avg_fitness)
-    print(f"Epoka 0, najlepsze przystosowanie: {best_individual.fitness}")
+    best_individual, best_fitness_history, avg_fitness_history = population.run(evaluate_fitness, bounds)
 
-    for generation in range(1, num_epochs + 1):
-        if selection_method == "best":
-            selected = best_selection(population, config.best_selection_amount)
-        elif selection_method == "roulette":
-            selected = roulette_selection(population, population_size)
-        else:
-            selected = tournament_selection(population, tournament_size=config.tournament_size)
+    for i in range(1, len(best_fitness_history)):
+        plotter.update_history(best_fitness_history[i], avg_fitness_history[i])
 
-        if config.optimization_type == "max":
-            elites = sorted(population, key=lambda ind: ind.fitness, reverse=True)[:elitism_count]
-        else:
-            elites = sorted(population, key=lambda ind: ind.fitness)[:elitism_count]
-
-        new_population = []
-
-        while len(new_population) < population_size - elitism_count:
-            parent1 = random.choice(selected)
-            parent2 = random.choice(selected)
-
-            if random.random() < crossover_rate:
-
-                child1 = Individual(num_variables=num_variables, precision=precision, random_init=False)
-                child2 = Individual(num_variables=num_variables, precision=precision, random_init=False)
-
-                parent1_values = parent1.chromosome_values
-                parent2_values = parent2.chromosome_values
-
-
-                if crossover_method == "arithmetic":
-                    child1_values, child2_values = arithmetic_crossover(parent1_values, parent2_values)
-                elif crossover_method == "linear":
-                    child1_values, child2_values = linear_crossover(parent1_values, parent2_values)
-                elif crossover_method == "blend_alpha":
-                    child1_values, child2_values = blend_alpha_crossover(parent1_values, parent2_values)
-                elif crossover_method == "blend_alpha_beta":
-                    child1_values, child2_values = blend_alpha_beta_crossover(parent1_values, parent2_values)
-                elif crossover_method == "averaging":
-                    child1_values, child2_values = averaging_crossover(parent1_values, parent2_values)
-                else:
-                    child1_values, child2_values = arithmetic_crossover(parent1_values, parent2_values)
-
-                child1.chromosomes[0].set_chromosome(child1_values)
-                child2.chromosomes[0].set_chromosome(child2_values)
-
-                if len(new_population) < population_size - elitism_count:
-                    new_population.append(child1)
-                if len(new_population) < population_size - elitism_count:
-                    new_population.append(child2)
-            else:
-                if len(new_population) < population_size - elitism_count:
-                    child1 = Individual(num_variables=num_variables, precision=precision, random_init=False)
-                    child1.chromosomes[0].set_chromosome(parent1.chromosome_values)
-                    new_population.append(child1)
-                if len(new_population) < population_size - elitism_count:
-                    child2 = Individual(num_variables=num_variables, precision=precision, random_init=False)
-                    child2.chromosomes[0].set_chromosome(parent2.chromosome_values)
-                    new_population.append(child2)
-
-        for ind in new_population:
-            ind.apply_mutation(mutation_rate, method=config.mutation_method)
-
-        new_population.extend(elites)
-
-        for ind in new_population:
-            ind.evaluate(evaluate_fitness)
-
-        population = new_population
-
-        current_best = optimization_func(population, key=lambda ind: ind.fitness)
-        avg_fitness = sum(ind.fitness for ind in population) / len(population)
-        plotter.update_history(current_best.fitness, avg_fitness)
-
-        if (config.optimization_type == "max" and current_best.fitness > best_individual.fitness) or \
-                (config.optimization_type == "min" and current_best.fitness < best_individual.fitness):
-            best_individual = current_best
-
-        if generation % 10 == 0:
-            print(f"Epoka {generation}, najlepsze przystosowanie: {best_individual.fitness}")
-
-    best_individual.evaluate(evaluate_fitness)
     end_time = time.time()
     execution_time = end_time - start_time
     print(f"Czas wykonania algorytmu: {execution_time:.2f} sekund")
--- algorithms/population.py
+++ algorithms/population.py
@@ -1,132 +1,125 @@
 import random
-import numpy as np
 from algorithms.individual import Individual
-from algorithms.mutation import edge_mutation, single_point_mutation, two_point_mutation
 from algorithms.selection import best_selection, roulette_selection, tournament_selection
-from algorithms.crossover import single_point_crossover, two_point_crossover, uniform_crossover
-from config import Config
-from algorithms.chromosome import Chromosome
+from algorithms.crossover import arithmetic_crossover, linear_crossover, blend_alpha_crossover, \
+    blend_alpha_beta_crossover, averaging_crossover
+from algorithms.config import config
+from algorithms.fitness import evaluate_fitness
+
 
 class Population:
-    def __init__(self, config: Config):
+    def __init__(self, config=config):
         self.config = config
         self.size = config.population_size
         self.num_variables = config.num_variables
-        self.bits_per_variable = config.precision
         self.precision = config.precision
         self.generation = 0
-
         self.individuals = [
-            Individual(self.num_variables, self.bits_per_variable, self.precision)
+            Individual(num_variables=self.num_variables, precision=self.precision)
             for _ in range(self.size)
         ]
+        self.best_individual = None
+        self.best_fitness_history = []
+        self.avg_fitness_history = []
 
-    def evaluate_all(self, fitness_func, bounds):
+    def evaluate_all(self, fitness_func=evaluate_fitness, bounds=None):
+        if bounds is None:
+            bounds = (self.config.range_start, self.config.range_end)
+
         for individual in self.individuals:
             individual.evaluate(fitness_func, bounds)
 
-    def select(self):
+        if self.config.optimization_type == "max":
+            self.best_individual = max(self.individuals, key=lambda ind: ind.fitness)
+        else:
+            self.best_individual = min(self.individuals, key=lambda ind: ind.fitness)
+
+        avg_fitness = sum(ind.fitness for ind in self.individuals) / self.size
+        self.best_fitness_history.append(self.best_individual.fitness)
+        self.avg_fitness_history.append(avg_fitness)
+
+    def select_parents(self):
         method = self.config.selection_method
 
         if method == "tournament":
-            return random.choice(tournament_selection(self.individuals, self.config.tournament_size))
+            selected = tournament_selection(self.individuals, tournament_size=self.config.tournament_size)
         elif method == "best":
-            return random.choice(best_selection(self.individuals, self.config.best_selection_amount))
+            selected = best_selection(self.individuals, self.config.best_selection_amount)
         elif method == "roulette":
-            return random.choice(roulette_selection(self.individuals, self.config.best_selection_amount))
+            selected = roulette_selection(self.individuals, self.size)
         else:
             raise ValueError(f"Unknown selection method: {method}")
 
+        return selected
+
     def crossover(self, parent1, parent2):
         method = self.config.crossover_method
-        offspring1 = Individual(self.num_variables, self.bits_per_variable, self.precision, False)
-        offspring2 = Individual(self.num_variables, self.bits_per_variable, self.precision, False)
 
-        for i in range(self.num_variables):
-            if parent1.chromosomes[i] and parent2.chromosomes[i]:
-                if method == "single_point":
-                    child1_chrom, child2_chrom = single_point_crossover(
-                        parent1.chromosomes[i], parent2.chromosomes[i]
-                    )
-                elif method == "two_point":
-                    child1_chrom, child2_chrom = two_point_crossover(
-                        parent1.chromosomes[i], parent2.chromosomes[i]
-                    )
-                elif method == "uniform":
-                    child1_chrom, child2_chrom = uniform_crossover(
-                        parent1.chromosomes[i], parent2.chromosomes[i]
-                    )
-                else:
-                    raise ValueError(f"Unknown crossover method: {method}")
-            
-                if child1_chrom is not None and child2_chrom is not None:
-                    offspring1.chromosomes[i] = child1_chrom
-                    offspring2.chromosomes[i] = child2_chrom
-                else:
-                    offspring1.chromosomes[i] = Chromosome(parent1.chromosomes[i].get_chromosome_len(), False)
-                    offspring1.chromosomes[i].set_chromosome(parent1.chromosomes[i].chromosome.copy())
-                
-                    offspring2.chromosomes[i] = Chromosome(parent2.chromosomes[i].get_chromosome_len(), False)
-                    offspring2.chromosomes[i].set_chromosome(parent2.chromosomes[i].chromosome.copy())
+        child1 = Individual(num_variables=self.num_variables, precision=self.precision, random_init=False)
+        child2 = Individual(num_variables=self.num_variables, precision=self.precision, random_init=False)
 
-        return offspring1, offspring2
+        parent1_values = parent1.chromosome_values
+        parent2_values = parent2.chromosome_values
 
+        if method == "arithmetic":
+            child1_values, child2_values = arithmetic_crossover(parent1_values, parent2_values)
+        elif method == "linear":
+            child1_values, child2_values = linear_crossover(parent1_values, parent2_values)
+        elif method == "blend_alpha":
+            child1_values, child2_values = blend_alpha_crossover(parent1_values, parent2_values)
+        elif method == "blend_alpha_beta":
+            child1_values, child2_values = blend_alpha_beta_crossover(parent1_values, parent2_values)
+        elif method == "averaging":
+            child1_values, child2_values = averaging_crossover(parent1_values, parent2_values)
+        else:
+            child1_values, child2_values = arithmetic_crossover(parent1_values, parent2_values)
 
-    def mutate(self, individual: Individual):
-        for chrom in individual.chromosomes:
-            method = self.config.mutation_method
-            if method == "edge":
-                edge_mutation(chrom, self.config.mutation_probability)
-            elif method == "single_point":
-                single_point_mutation(chrom, self.config.mutation_probability)
-            elif method == "two_point":
-                two_point_mutation(chrom, self.config.mutation_probability)
-            else:
-                raise ValueError(f"Unknown mutation method: {method}")
+        child1.chromosomes[0].set_chromosome(child1_values)
+        child2.chromosomes[0].set_chromosome(child2_values)
 
-    def evolve(self, fitness_func, bounds):
-        self.evaluate_all(fitness_func, bounds)
-        reverse_sort = self.config.optimization_type == "max"
-        self.individuals.sort(key=lambda ind: ind.fitness, reverse=reverse_sort)
+        return child1, child2
+
+    def evolve(self):
+        selected = self.select_parents()
+        if self.config.optimization_type == "max":
+            elites = sorted(self.individuals, key=lambda ind: ind.fitness, reverse=True)[
+                     :self.config.best_selection_amount]
+        else:
+            elites = sorted(self.individuals, key=lambda ind: ind.fitness)[:self.config.best_selection_amount]
 
         new_population = []
 
-        if self.config.elite_size > 0:
-            new_population.extend(self.individuals[:self.config.elite_size])
-
-        while len(new_population) < self.size:
-            parent1 = self.select()
-            parent2 = self.select()
+        while len(new_population) < self.size - len(elites):
+            parent1 = random.choice(selected)
+            parent2 = random.choice(selected)
 
             if random.random() < self.config.crossover_probability:
-                offspring1, offspring2 = self.crossover(parent1, parent2)
+                child1, child2 = self.crossover(parent1, parent2)
             else:
-                offspring1, offspring2 = parent1, parent2
+                child1 = Individual(num_variables=self.num_variables, precision=self.precision, random_init=False)
+                child2 = Individual(num_variables=self.num_variables, precision=self.precision, random_init=False)
+                child1.chromosomes[0].set_chromosome(parent1.chromosome_values)
+                child2.chromosomes[0].set_chromosome(parent2.chromosome_values)
 
-            self.mutate(offspring1)
-            self.mutate(offspring2)
+            child1.apply_mutation(self.config.mutation_probability, method=self.config.mutation_method)
+            child2.apply_mutation(self.config.mutation_probability, method=self.config.mutation_method)
 
-            new_population.append(offspring1)
-            if len(new_population) < self.size:
-                new_population.append(offspring2)
+            if len(new_population) < self.size - len(elites):
+                new_population.append(child1)
+            if len(new_population) < self.size - len(elites):
+                new_population.append(child2)
 
-        self.individuals = new_population[:self.size]
+        new_population.extend(elites)
+        self.individuals = new_population
         self.generation += 1
 
-    def run(self, fitness_func, bounds):
-        best_fitness_history = []
-        avg_fitness_history = []
-
+    def run(self, fitness_func=evaluate_fitness, bounds=None):
         for gen in range(self.config.epochs):
-            self.evolve(fitness_func, bounds)
+            self.evaluate_all(fitness_func, bounds)
+            self.evolve()
 
-            best_fitness = min(ind.fitness for ind in self.individuals) if self.config.optimization_type == "min" else max(ind.fitness for ind in self.individuals)
-            avg_fitness = sum(ind.fitness for ind in self.individuals) / self.size
+            if gen % 10 == 0:
+                print(f"Epoka {gen}, najlepsze przystosowanie: {self.best_individual.fitness}")
 
-            best_fitness_history.append(best_fitness)
-            avg_fitness_history.append(avg_fitness)
-
-            print(f"Pokolenie {gen+1}/{self.config.epochs}: Najlepsze przystosowanie = {best_fitness:.6f}, Średnie przystosowanie = {avg_fitness:.6f}")
-
-        best_individual = min(self.individuals, key=lambda ind: ind.fitness) if self.config.optimization_type == "min" else max(self.individuals, key=lambda ind: ind.fitness)
-        return best_individual, best_fitness_history, avg_fitness_history
+        self.evaluate_all(fitness_func, bounds)
+        return self.best_individual, self.best_fitness_history, self.avg_fitness_history
